2c435a48b2701de7734407a6c11f15483943011b
Rümeysa AYAZ <rumeysadurmus_@hotmail.com>
2023-10-24 23:37:54 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Value Types
+        double number5 = 10.4;
+        decimal number6 = 10.4m;
+        char character = 'A';
+        bool condition = false;
+        byte number4 = 255;
+        short number3 = 32767;
+        int number1 = 214748364;
+        long number2 = 9223372036854775807;
+        var number7 = 10;
+        number7 = 'A';
+
+        Console.WriteLine("Number1 is {0}", number1);
+        Console.WriteLine("Number2 is {0}", number2);
+        Console.WriteLine("Number3 is {0}", number3);
+        Console.WriteLine("Number4 is {0}", number4);
+        Console.WriteLine("Number5 is {0}", number5);
+        Console.WriteLine("Number6 is {0}", number6);
+        Console.WriteLine("Number7 is {0}", number7);
+        Console.WriteLine("Character is : {0}", (int)character);
+        Console.WriteLine((int)Days.Friday);
+        Console.ReadLine();
+
+    }
+}
+
+enum Days
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
