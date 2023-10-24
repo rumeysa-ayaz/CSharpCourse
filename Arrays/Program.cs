652818fa6f11e66449dab6e9b00d9109fd238d9c
Rümeysa AYAZ <rumeysadurmus_@hotmail.com>
2023-10-25 00:33:13 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Arrays
+        //string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+        //string[] students2 = { "Engin", "Derin", "Salih" }; 
+
+        //foreach (var student in students)
+        //{
+        //    Console.WriteLine(student);
+        //}
+
+        //Multidimensional Arrays
+        string[,] regions = new string[7, 3]
+        {
+            {"İstanbul","Bursa","Yalova" },
+            {"Ankara","Sivas","Konya" },
+            {"Antalya","Adana","Mersin" },
+            {"Trabzon","Samsun","Rize" },
+            {"İzmir","Muğla","Aydın" },
+            {"Erzincan","Malatya","Van" },
+            {"Diyarbakır","Gaziantep","Şanlıurfa" },
+        };
+
+        for (int i = 0; i <= regions.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= regions.GetUpperBound(1); j++)
+            {
+                Console.WriteLine(regions[i, j]);
+            }
+            Console.WriteLine("*********");
+        }
+
+        Console.ReadLine();
+
+    }
+}
