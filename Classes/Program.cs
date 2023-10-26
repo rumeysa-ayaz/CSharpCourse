eecb47bd4227c29fc2af969f93c40ea54ba9975b
Rümeysa AYAZ <rumeysadurmus_@hotmail.com>
2023-10-26 10:38:10 +0300

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using Classes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Classes
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Update();
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add();
+        productManager.Update();
+
+        Customer customer = new Customer();
+        customer.City = "İstanbul";
+        customer.Id = 1;
+        customer.FirstName = "Rümeysa";
+        customer.LastName = "Furkan";
+
+        Customer customer2 = new Customer
+        {
+            Id = 2, City = "Bursa", FirstName ="Furkan", LastName ="Ayaz"
+        };
+
+        Console.WriteLine(customer2.FirstName);
+
+        Console.ReadLine();
+    }
+}
